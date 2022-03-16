0b4f2bd43b970c2be3f1e5873c376d712fc03c29
olajide <olajideidowu41@gmail.com>
2022-03-17 00:55:20 +0100

    Area of Circle

---
+++ Example/Circle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle.Example
+{
+        class Circle
+        {
+            double pi;
+            double radius;
+
+            public Circle(double pi, double radius)
+            {
+                this.pi = pi;
+                this.radius = radius;
+            }
+
+        public Circle()
+        {
+
+        }
+
+
+            public void AcceptDetails()
+            {
+                pi = Math.PI;
+                radius = 7;
+            }
+
+            public double CalculateArea()
+            {
+                return pi * Math.Pow(radius,2);
+            }
+
+            public void DisplayResult()
+            {
+                Console.WriteLine("Area {0}", CalculateArea());
+            }
+
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Circle.Example;
+
+namespace Circle.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle();
+            circle.AcceptDetails();
+            circle.CalculateArea();
+            circle.DisplayResult();
+            Console.ReadLine();
+        }
+    }
+}
