d285d387e54bb149e1779743905261b4bb945a4e
olajide <olajideidowu41@gmail.com>
2022-03-24 09:16:26 +0100

    conditional operator

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Condition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string x = "1747";
+            var y = int.Parse(x);
+
+            double z = y / 100;
+
+            var a = z % 10;
+            var b = x[1];
+            
+
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(x[1]);
+
+            Console.WriteLine(a == 7 && x[1] == 7 ? "You are correct": "You are wrong");
+            if(a==7 && b == 7)
+            {
+                Console.WriteLine("You are Correct");
+            }
+            else
+            {
+                Console.WriteLine("You are wrong");
+            }
+
+
+        }
+    }
+}
+
+
